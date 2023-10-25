4ffed43d3424fed7a10288dc75127cd6089b14a0
ostmax31 <26828867-ostmax31@users.noreply.replit.com>
2023-10-25 18:07:30 +0000

    Initial commit

---
+++ .ccls-cache/@home@runner@TornWholeDeclarations/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <stdio.h>
+using namespace std;
+int main() 
+{
+  printf()
+  scanf("%li %lf");
+  double a,b;
+cout << "Введiть два числа \n";
+  cin >> a >> b;
+  printf("Сума цих чисел - "<< a + b << endl;)
+    cout <<"Різниця цих чисел - "<< a - b << endl;
+  return 0;
+
+}  
+ 
